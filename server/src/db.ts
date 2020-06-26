57bd03e01322fa9dd60c01f7a801bb07774f25be
pavl0sh <duckdogers07@gmail.com>
2020-06-26 18:33:19 +0300

    prepare for heroku deploy

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,12 +1,11 @@
 import * as admin from 'firebase-admin';
 import 'dotenv/config';
-import serviceAccount from './service-account.json';
 
 admin.initializeApp({
     credential: admin.credential.cert({
-        privateKey: serviceAccount.private_key,
-        clientEmail: serviceAccount.client_email,
-        projectId: serviceAccount.project_id,
+        privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+        clientEmail: process.env.CLIENT_EMAIL,
+        projectId: process.env.PROJECT_ID,
     }),
     databaseURL: process.env.FIRESTORE_DB,
 });
